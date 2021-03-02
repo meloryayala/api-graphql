1283ef75f59d7c11acfe0092c29dfeafea73eb6e
Melory Ayala <melory.ayala@gmail.com>
2021-03-02 11:00:02 +0100

    Connecting to database

--- src/startServer.js
+++ src/startServer.js
@@ -1,6 +1,12 @@
 import {ApolloServer} from "apollo-server";
+import mongoose from "mongoose";
 
 function startServer({ typeDefs, resolvers}) {
+    mongoose.connect('mongodb://127.0.0.1/graphql', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+
     const server = new ApolloServer({typeDefs, resolvers});
     server.listen().then(({url}) => console.log(`🏁 Server started at ${url}`));
 }
